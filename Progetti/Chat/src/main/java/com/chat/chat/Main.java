7433d14cbbfe5501db7ba087f8e3ae27401a1aa7
Lorenzo Gambaro <lorygamba06@gmail.com>
2024-01-24 10:50:14 +0100

    Finalizzazione Progetti Chat e CartaConto

--- Progetti/Chat/src/main/java/com/chat/chat/Main.java
+++ Progetti/Chat/src/main/java/com/chat/chat/Main.java
@@ -20,14 +20,21 @@ public class Main
     {
         try
         {          
-            final Messanger c =  new Messanger(2233);
+            final Scanner sc2 = new Scanner(System.in);
+            
+            System.out.println("Inserisci la porta alla quale vuoi connetterti: ");
+            
+            final Messanger c =  new Messanger(sc2.nextInt());
             c.startReciving((final DatagramPacket packet) -> 
             {
                 System.out.println(new String(packet.getData(), 0, packet.getLength())); 
             });
+            sc2.nextLine();
+            System.out.println("Inserisci l'indirizzo al quale vuoi connetterti: ");
+            final String add = sc2.nextLine();
+            
             System.out.println("Benvenuto nella chat, inserisci il tuo nome: ");
             
-            final Scanner sc2 = new Scanner(System.in);
             
             StringBuilder sb = new StringBuilder();
 //
@@ -37,7 +44,7 @@ public class Main
             String line;
             while (sc2.hasNextLine() && !(line = sc2.nextLine()).equals("END"))
             {
-                c.send(name+ ": " + line, InetAddress.getByName("192.168.4.255"));
+                c.send(name+ ": " + line, InetAddress.getByName(add));
                 Thread.sleep(1);
             }
             
